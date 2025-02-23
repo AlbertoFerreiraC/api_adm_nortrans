1046fe618c93c190104b88bd561299980f719713
Jennifer <jennifersoledad94@gmail.com>
2025-02-23 01:42:49 -0300

    mantenedores de cargo y tipo equipo

---
+++ tipoequipo/sql.php
@@ -0,0 +1,76 @@
+<?php
+
+include_once '../db.php';
+
+class Sql extends DB
+{
+
+
+  function listarHerramientas()
+  {
+    $query = $this->connect()->prepare("select * from tipo_bus where estado = 'activo'");
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function verificar_existencia($item)
+  {
+    $query = $this->connect()->prepare("select * from tipo_bus where estado = 'activo' and 
+        descripcion = :descripcion");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function agregar($item)
+  {
+    $query = $this->connect()->prepare("insert tipo_bus(descripcion,estado) values(:descripcion,'activo')");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+
+  function obtenerDatosParaModificar($item)
+  {
+    $query = $this->connect()->prepare("select * from tipo_bus where estado = 'activo' and 
+      idtipo_bus = :id");
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function modificar($item)
+  {
+    $query = $this->connect()->prepare("update tipo_bus set descripcion = :descripcion where idtipo_bus = :id and estado = 'activo'");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+
+  function eliminar($item)
+  {
+    $query = $this->connect()->prepare("update tipo_bus set estado = 'inactivo' where idtipo_bus = :id and estado = 'activo'");
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+}
