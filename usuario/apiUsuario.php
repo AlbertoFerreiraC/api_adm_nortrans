8e4076ab8e83e15b02409ee1f82930e3af52b078
aferreira <albertof6064@gmail.com>
2025-02-24 18:42:28 -0300

    correccion de insercion solicitud contratacion

--- usuario/apiUsuario.php
+++ usuario/apiUsuario.php
@@ -2,171 +2,193 @@
 
 include_once 'usuario.php';
 
-class ApiUsuario{
-   
-    function listarApi(){
+class ApiUsuario
+{
+
+    function listarApi()
+    {
         $objeto = new Usuario();
-        $lista = $objeto->listar();      
+        $lista = $objeto->listar();
         $listaArr = array();
-        if(!empty($lista)){
+        if (!empty($lista)) {
             foreach ($lista as $clave => $valor) {
                 $item = array(
-                   'idusuario'=> $valor['idusuario'],
-                   'rol'=> $valor['rol'],
-                   'cedula'=> $valor['cedula'],
-                   'nombre'=> $valor['nombre'],
-                   'nic'=> $valor['nic'],
-                   'telefono'=> $valor['telefono'],
-                   'correo'=> $valor['correo'],
-                   'preAprueba'=> $valor['pre_aprueba'],
-                   'aprueba'=> $valor['aprueba']
-               );
-               array_push($listaArr, $item);               
-           }
-           printJSON($listaArr);
-        }else{
+                    'idusuario' => $valor['idusuario'],
+                    'rol' => $valor['rol'],
+                    'cedula' => $valor['cedula'],
+                    'nombre' => $valor['nombre'],
+                    'nic' => $valor['nic'],
+                    'telefono' => $valor['telefono'],
+                    'correo' => $valor['correo'],
+                    'preAprueba' => $valor['pre_aprueba'],
+                    'aprueba' => $valor['aprueba']
+                );
+                array_push($listaArr, $item);
+            }
+            printJSON($listaArr);
+        } else {
             //error("error");
             header("HTTP/1.1 401 Unauthorized");
         }
-    } 
+    }
 
+    function listarApruebaApi()
+    {
+        $objeto = new Usuario();
+        $lista = $objeto->listar();
+        $listaArr = array();
+        if (!empty($lista)) {
+            foreach ($lista as $clave => $valor) {
+                $item = array(
+                    'nombre' => $valor['nombre']
+                );
+                array_push($listaArr, $item);
+            }
+            printJSON($listaArr);
+        } else {
+            //error("error");
+            header("HTTP/1.1 401 Unauthorized");
+        }
+    }
 
-    function agregarApi($array){
+    function agregarApi($array)
+    {
         $objeto = new Usuario();
         //********************************************************************    
         $verificarExistenciacedula = $objeto->verificar_existencia_cedula($array);
-        if(empty($verificarExistenciacedula)){
+        if (empty($verificarExistenciacedula)) {
             $verificarExistenciaNic = $objeto->verificar_existencia_nic($array);
-            if(empty($verificarExistenciaNic)){
-                    $guardar = $objeto->agregar($array);
-                    if($guardar == "ok"){
-                        exito("ok");
-                    }else{
-                        exito("nok");
-                    }           
-            }else{
+            if (empty($verificarExistenciaNic)) {
+                $guardar = $objeto->agregar($array);
+                if ($guardar == "ok") {
+                    exito("ok");
+                } else {
+                    exito("nok");
+                }
+            } else {
                 error("registro_existente");
-            }             
-         
-        }else{
+            }
+        } else {
             error("registro_existente");
         }
     }
 
-    function obtenerDatosParaModificarApi($array){
+    function obtenerDatosParaModificarApi($array)
+    {
         $objeto = new Usuario();
-        $lista = $objeto->obtenerDatosParaModificar($array);      
+        $lista = $objeto->obtenerDatosParaModificar($array);
         $listaArr = array();
-        if(!empty($lista)){
+        if (!empty($lista)) {
             foreach ($lista as $clave => $valor) {
                 $item = array(
-                   'id'=> $valor['idusuario'],
-                   'rol'=> $valor['rol'],
-                   'cedula'=> $valor['cedula'],
-                   'nombre'=> $valor['nombre'],
-                   'nic'=> $valor['nic'],
-                   'telefono'=> $valor['telefono'],
-                   'correo'=> $valor['correo'],
-                   'preAprueba'=> $valor['pre_aprueba'],
-                   'aprueba'=> $valor['aprueba']
-               );
-               array_push($listaArr, $item);               
-           }
-           printJSON($listaArr);
-        }else{
+                    'id' => $valor['idusuario'],
+                    'rol' => $valor['rol'],
+                    'cedula' => $valor['cedula'],
+                    'nombre' => $valor['nombre'],
+                    'nic' => $valor['nic'],
+                    'telefono' => $valor['telefono'],
+                    'correo' => $valor['correo'],
+                    'preAprueba' => $valor['pre_aprueba'],
+                    'aprueba' => $valor['aprueba']
+                );
+                array_push($listaArr, $item);
+            }
+            printJSON($listaArr);
+        } else {
             //error("error");
             header("HTTP/1.1 401 Unauthorized");
         }
-    } 
+    }
 
-    function modificarApi($array){
+    function modificarApi($array)
+    {
         $objeto = new Usuario();
         //******************************************************************** 
-        $controlActualizacion = 1;   
+        $controlActualizacion = 1;
 
         $verificarExistenciaCedula = $objeto->verificar_existencia_cedula($array);
-        if(!empty($verificarExistenciaCedula)){
+        if (!empty($verificarExistenciaCedula)) {
             $idRecogido = $verificarExistenciaCedula[0]['idusuario'];
             $idParaModificar = $array['id'];
-            if($idRecogido != $idParaModificar){
-                $controlActualizacion = 2;                                   
+            if ($idRecogido != $idParaModificar) {
+                $controlActualizacion = 2;
             }
         }
 
         $verificarExistenciaNic = $objeto->verificar_existencia_nic($array);
-        if(!empty($verificarExistenciaNic)){
+        if (!empty($verificarExistenciaNic)) {
             $idRecogido = $verificarExistenciaNic[0]['idusuario'];
             $idParaModificar = $array['id'];
-            if($idRecogido != $idParaModificar){
-                $controlActualizacion = 3;               
-            } 
+            if ($idRecogido != $idParaModificar) {
+                $controlActualizacion = 3;
+            }
         }
 
-        if($controlActualizacion == 1){
-            $datos = array( 
-                'id'=> $array['id'],
-                'rol'=> $array['rol'],
-                'cedula'=> $array['cedula'],
-                'nombre'=> $array['nombre'],
-                'nic'=> $array['nic'],
-                'telefono'=> $array['telefono'],
-                'correo'=> $array['correo'],
-                'preAprueba'=> $array['preAprueba'],
-                'aprueba'=> $array['aprueba']
+        if ($controlActualizacion == 1) {
+            $datos = array(
+                'id' => $array['id'],
+                'rol' => $array['rol'],
+                'cedula' => $array['cedula'],
+                'nombre' => $array['nombre'],
+                'nic' => $array['nic'],
+                'telefono' => $array['telefono'],
+                'correo' => $array['correo'],
+                'preAprueba' => $array['preAprueba'],
+                'aprueba' => $array['aprueba']
             );
             $editar = $objeto->modificar($datos);
-            if($editar == "ok"){
+            if ($editar == "ok") {
                 exito("ok");
-            }else{
+            } else {
                 exito("nok");
-            } 
+            }
         }
 
-        if($controlActualizacion == 2){
+        if ($controlActualizacion == 2) {
             exito("registro_existente");
         }
-        
-        if($controlActualizacion == 3){
+
+        if ($controlActualizacion == 3) {
             exito("registro_existente");
         }
     }
 
-    function eliminarApi($array){
+    function eliminarApi($array)
+    {
         $objeto = new Usuario();
         //********************************************************************    
         $eliminar = $objeto->eliminar($array);
-                if($eliminar == "ok"){
-                    exito("ok");
-                }else{
-                    exito("nok");
-                } 
+        if ($eliminar == "ok") {
+            exito("ok");
+        } else {
+            exito("nok");
+        }
     }
 
-    function resetearPassApi($array){
+    function resetearPassApi($array)
+    {
         $objeto = new Usuario();
         //********************************************************************    
         $eliminar = $objeto->resetearPass($array);
-                if($eliminar == "ok"){
-                    exito("ok");
-                }else{
-                    exito("nok");
-                } 
+        if ($eliminar == "ok") {
+            exito("ok");
+        } else {
+            exito("nok");
+        }
     }
-
-   
-
 } //FIN API SESIONES
 
-    function error($mensaje){
-        echo json_encode(array('mensaje' => $mensaje)); 
-    }
+function error($mensaje)
+{
+    echo json_encode(array('mensaje' => $mensaje));
+}
 
-    function exito($mensaje){
-        echo json_encode(array('mensaje' => $mensaje)); 
-    }
+function exito($mensaje)
+{
+    echo json_encode(array('mensaje' => $mensaje));
+}
 
-    function printJSON($array){
-        echo json_encode($array);
-    }
-
-?>
+function printJSON($array)
+{
+    echo json_encode($array);
+}
