934a0b1c0db8ae9db355bea9ce929b0c5b03f2ff
aferreira <albertof6064@gmail.com>
2025-03-06 15:29:38 -0300

    aprueba finalizado

---
+++ aprueba/controlador.php
@@ -0,0 +1,156 @@
+<?php
+
+include_once 'sql.php';
+
+class ApiControlador
+{
+
+    function listarHerramientasApi($array)
+    {
+        $clasificacion = new Sql();
+        $lista = $clasificacion->listarHerramientas($array);
+        $listaArr = array();
+        if (!empty($lista)) {
+            foreach ($lista as $clave => $valor) {
+                $item = array(
+                    'idcontratacion' => $valor['idcontratacion'],
+                    'cargo' => $valor['cargo'],
+                    'empresa' => $valor['empresa'],
+                    'centro_de_costo' => $valor['centro_de_costo'],
+                    'turnos_laborales' => $valor['turnos_laborales'],
+                    'tipo_bus' => $valor['tipo_bus'],
+                    'pre_aprueba' => $valor['pre_aprueba'],
+                    'aprueba' => $valor['aprueba'],
+                    'motivo' => $valor['motivo'],
+                    'tipo_contrato' => $valor['tipo_contrato'],
+                    'division' => $valor['division'],
+                    'cantidad_solicitada' => $valor['cantidad_solicitada'],
+                    'licencia_de_conducir' => $valor['licencia_de_conducir'],
+                    'fecha_requerida' => $valor['fecha_requerida'],
+                    'fecha_termino' => $valor['fecha_termino'],
+                    'remuneracion' => $valor['remuneracion'],
+                    'comentario_general' => $valor['comentario_general'],
+                    'estado' => 'activo',
+                    'observacionEntrevistaPsicolaboral' => $valor['entrevista_psicolaboral'],
+                    'observacionEntrevistaTecnica' => $valor['entrevista_tecnica'],
+                    'observacionPruebaConduccion' => $valor['entrevista_conduccion']
+                );
+                array_push($listaArr, $item);
+            }
+            printJSON($listaArr);
+        } else {
+            //error("error");
+            header("HTTP/1.1 401 Unauthorized");
+        }
+    }
+
+    function agregarApi($array)
+    {
+        $clasificacion = new Sql();
+        //********************************************************************    
+        $verificarExistencia = $clasificacion->verificar_existencia($array);
+        if (empty($verificarExistencia)) {
+            $guardar = $clasificacion->agregar($array);
+            if ($guardar == "ok") {
+                exito("ok");
+            } else {
+                exito("nok");
+            }
+        }
+    }
+
+    /* function obtenerDatosParaModificarApi($array)
+    {
+        $clasificacion = new Sql();
+        $lista = $clasificacion->obtenerDatosParaModificar($array);
+        $listaArr = array();
+        if (!empty($lista)) {
+            foreach ($lista as $clave => $valor) {
+                $item = array(
+                    'idcontratacion' => $valor['idcontratacion'],
+                    'cargo' => $valor['cargo'],
+                    'empresa' => $valor['empresa'],
+                    'division' => $valor['division'],
+                    'centroDeCosto' => $valor['centro_de_costo'],
+                    'turnosLaborales' => $valor['turnos_laborales'],
+                    'tipoBus' => $valor['tipo_bus'],
+                    'preaprueba' => $valor['pre_aprueba'],
+                    'aprueba' => $valor['aprueba'],
+                    'motivo' => $valor['motivo'],
+                    'cantidad_solicitada' => $valor['cantidad_solicitada'],
+                    'licenciaDeConducir' => $valor['licencia_de_conducir'],
+                    'tipo_contrato' => $valor['tipo_contrato'],
+                    'fecha_requerida' => $valor['fecha_requerida'],
+                    'fecha_termino' => $valor['fecha_termino'],
+                    'remuneracion' => $valor['remuneracion'],
+                    'comentario_general' => $valor['comentario_general'],
+                    'aprueba' => $valor['aprueba'],
+                    'pre_aprueba' => $valor['pre_aprueba'],
+                    'observacionEntrevistaPsicolaboral' => $valor['entrevista_psicolaboral'],
+                    'observacionEntrevistaTecnica' => $valor['entrevista_tecnica'],
+                    'observacionPruebaConduccion' => $valor['entrevista_conduccion']
+                );
+                array_push($listaArr, $item);
+            }
+            printJSON($listaArr);
+        } else {
+            //error("error");
+            header("HTTP/1.1 401 Unauthorized");
+        }
+    }*/
+
+    function modificarApi($array)
+    {
+        $clasificacion = new Sql();
+        //********************************************************************    
+        $verificarExistencia = $clasificacion->verificar_existencia($array);
+        if (empty($verificarExistencia)) {
+            $editar = $clasificacion->modificar($array);
+            if ($editar == "ok") {
+                exito("ok");
+            } else {
+                exito("nok");
+            }
+        } else {
+            $idRecogido = $verificarExistencia[0]['idcontratacion'];
+            $idParaModificar = $array['idcontratacion'];
+            if ($idRecogido != $idParaModificar) {
+                exito("repetido");
+            } else {
+                $editar = $clasificacion->modificar($array);
+                if ($editar == "ok") {
+                    exito("ok");
+                } else {
+                    exito("nok");
+                }
+            }
+        }
+    }
+
+
+    function eliminarApi($array)
+    {
+        $clasificacion = new Sql();
+        $eliminar = $clasificacion->eliminar($array);
+        if ($eliminar == "ok") {
+            exito("ok");
+        } else {
+            exito("nok");
+        }
+    }
+} //FIN API SESIONES
+
+function error($mensaje)
+{
+    echo json_encode(array('mensaje' => $mensaje));
+}
+
+function exito($mensaje)
+{
+    echo json_encode(array('mensaje' => $mensaje));
+}
+
+function printJSON($array)
+{
+    echo json_encode($array);
+}
---
+++ aprueba/sql.php
@@ -0,0 +1,31 @@
+<?php
+
+include_once '../db.php';
+
+class Sql extends DB
+{
+
+  function listarHerramientas($item)
+  {
+    $query = $this->connect()->prepare("SELECT * FROM contratacion WHERE estado = 'activo' and aprueba = :id");
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function listarPorUsuario($id_usuario)
+  {
+    $query = $this->connect()->prepare("SELECT * FROM contratacion WHERE pre_aprueba = :id_usuario AND estado = 'activo'");
+    $query->bindParam(":id_usuario", $id_usuario, PDO::PARAM_INT);
+
+    if ($query->execute()) {
+      return $query->fetchAll(PDO::FETCH_ASSOC);
+    } else {
+      return null;
+    }
+  }
+}
+
