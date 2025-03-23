216ac8653ce42d6c340963b439415efb9f5836b5
aferreira <albertof6064@gmail.com>
2025-03-23 20:20:18 -0300

    backend tipo anexo

---
+++ tipoAnexo/sql.php
@@ -0,0 +1,76 @@
+<?php
+
+include_once '../db.php';
+
+class Sql extends DB
+{
+
+
+  function listarHerramientas()
+  {
+    $query = $this->connect()->prepare("select * from tipo_anexo where estado = 'activo'");
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function verificar_existencia($item)
+  {
+    $query = $this->connect()->prepare("select * from tipo_anexo where estado = 'activo' and 
+        descripcion = :descripcion");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function agregar($item)
+  {
+    $query = $this->connect()->prepare("insert tipo_anexo(descripcion,estado) values(:descripcion,'activo')");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+
+  function obtenerDatosParaModificar($item)
+  {
+    $query = $this->connect()->prepare("select * from tipo_anexo where estado = 'activo' and 
+      idtipo_anexo = :id");
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function modificar($item)
+  {
+    $query = $this->connect()->prepare("update tipo_anexo set descripcion = :descripcion where idtipo_anexo = :id and estado = 'activo'");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+
+  function eliminar($item)
+  {
+    $query = $this->connect()->prepare("update tipo_anexo set estado = 'inactivo' where idtipo_anexo = :id and estado = 'activo'");
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+}
