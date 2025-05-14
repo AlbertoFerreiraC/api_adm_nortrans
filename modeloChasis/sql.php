36eef3965c6e6df073da21b771c4caf0737e32ce
aferreira <albertof6064@gmail.com>
2025-05-14 13:58:37 -0300

    mantenedores faltantes

---
+++ modeloChasis/sql.php
@@ -0,0 +1,76 @@
+<?php
+
+include_once '../db.php';
+
+class Sql extends DB
+{
+
+
+  function listarHerramientas()
+  {
+    $query = $this->connect()->prepare("select * from modelo_chasis where estado = 'activo'");
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function verificar_existencia($item)
+  {
+    $query = $this->connect()->prepare("select * from modelo_chasis where estado = 'activo' and 
+        descripcion = :descripcion");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function agregar($item)
+  {
+    $query = $this->connect()->prepare("insert modelo_chasis(descripcion,estado) values(:descripcion,'activo')");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+
+  function obtenerDatosParaModificar($item)
+  {
+    $query = $this->connect()->prepare("select * from modelo_chasis where estado = 'activo' and 
+      idmodelo_chasis = :id");
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return $query->fetchAll();
+    } else {
+      return null;
+    }
+  }
+
+  function modificar($item)
+  {
+    $query = $this->connect()->prepare("update modelo_chasis set descripcion = :descripcion where idmodelo_chasis = :id and estado = 'activo'");
+    $query->bindParam(":descripcion", $item['descripcion'], PDO::PARAM_STR);
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+
+  function eliminar($item)
+  {
+    $query = $this->connect()->prepare("update modelo_chasis set estado = 'inactivo' where idmodelo_chasis = :id and estado = 'activo'");
+    $query->bindParam(":id", $item['id'], PDO::PARAM_STR);
+    if ($query->execute()) {
+      return "ok";
+    } else {
+      return "nok";
+    }
+  }
+}
